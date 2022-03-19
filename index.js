dfb804c5d1cf60c0527481f4dd16ef97eaf2f908
Lucas Rodrigues do Nascimento <lr.nas1894@gmail.com>
2022-03-19 00:39:06 -0300

    Decidi usar O Xampp.

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const option = require( "./src/Routes/option")
 const voto = require( "./src/Routes/voto")
 app.use(express.json());
 
-// const php = require('php')
-// const path = require('path')
-// app.set("views", path.join(__dirname,"/src/Views"))
-// app.set("view engine", php)
-// app.engine('php',php.__express)
 
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
@@ -22,12 +17,6 @@ app.use(enquete);
 app.use(option);
 app.use(voto);
 
-
-
-// app.get("/",(req,res)=>{
-//     res.render("teste.php", {var1:"teste",var2:"teste2"})
-// }
-// )
 
 app.listen(3030, () => {
     console.log("Server em execução!");
